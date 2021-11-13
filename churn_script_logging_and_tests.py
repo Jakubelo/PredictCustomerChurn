a62e33c87077f8e87db75b3f417a528746569e3c
jakub.galik <jakub.galik@sternkraft.com>
2021-11-13 17:08:04 +0100

    created test_eda - to finish, test_enconding module created

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -8,6 +8,32 @@ logging.basicConfig(
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
+cat_columns = [
+    'Gender',
+    'Education_Level',
+    'Marital_Status',
+    'Income_Category',
+    'Card_Category'                
+]
+
+quant_columns = [
+    'Customer_Age',
+    'Dependent_count', 
+    'Months_on_book',
+    'Total_Relationship_Count', 
+    'Months_Inactive_12_mon',
+    'Contacts_Count_12_mon', 
+    'Credit_Limit', 
+    'Total_Revolving_Bal',
+    'Avg_Open_To_Buy', 
+    'Total_Amt_Chng_Q4_Q1', 
+    'Total_Trans_Amt',
+    'Total_Trans_Ct', 
+    'Total_Ct_Chng_Q4_Q1', 
+    'Avg_Utilization_Ratio'
+]
+
+
 def test_import(import_data):
 	'''
 	test data import - this example is completed for you to assist with the other test functions
@@ -27,16 +53,69 @@ def test_import(import_data):
 		raise err
 
 
-def test_eda(perform_eda):
+def test_eda(df):
 	'''
 	test perform eda function
 	'''
+    df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+    X = pd.DataFrame()
+    gender_lst = []
+    gender_groups = df.groupby('Gender').mean()['Churn']
+
+    for val in df['Gender']:
+        gender_lst.append(gender_groups.loc[val])
+
+    df['Gender_Churn'] = gender_lst    
+    #education encoded column
+    edu_lst = []
+    edu_groups = df.groupby('Education_Level').mean()['Churn']
+
+    for val in df['Education_Level']:
+        edu_lst.append(edu_groups.loc[val])
+
+    df['Education_Level_Churn'] = edu_lst
+
+    #marital encoded column
+    marital_lst = []
+    marital_groups = df.groupby('Marital_Status').mean()['Churn']
+
+    for val in df['Marital_Status']:
+        marital_lst.append(marital_groups.loc[val])
+
+    df['Marital_Status_Churn'] = marital_lst
+
+    #income encoded column
+    income_lst = []
+    income_groups = df.groupby('Income_Category').mean()['Churn']
+
+    for val in df['Income_Category']:
+        income_lst.append(income_groups.loc[val])
+
+    df['Income_Category_Churn'] = income_lst
+
+    #card encoded column
+    card_lst = []
+    card_groups = df.groupby('Card_Category').mean()['Churn']
+
+    for val in df['Card_Category']:
+        card_lst.append(card_groups.loc[val])
+
+    df['Card_Category_Churn'] = card_lst
+    return df
 
 
-def test_encoder_helper(encoder_helper):
+def test_encoder_helper(df, category):
 	'''
 	test encoder helper
 	'''
+    result_list = []
+    res_group = df.groupby(category).mean()['Churn']
+
+    for val in df[category]:
+        result_list.append(res_group.loc[val])
+        
+    return result_list
+        
 
 
 def test_perform_feature_engineering(perform_feature_engineering):
